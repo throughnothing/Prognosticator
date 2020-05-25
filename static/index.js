19deebcb6f21b9da897465c8d3e27559d2d6972f
William Wolf <throughnothing@users.noreply.github.com>
2020-05-25 17:07:57 -0400

    Fix hot reloading duplicate issues with Halogen

--- static/index.js
+++ static/index.js
@@ -1,23 +1,11 @@
 var Main = require('../output/UI.Main');
 
 function main () {
-    /*
-    Here we could add variables such as
-
-    var baseUrl = process.env.BASE_URL;
-
-    Parcel will replace `process.env.BASE_URL`
-    with the string contents of the BASE_URL environment
-    variable at bundle/build time.
-    A .env file can also be used to override shell variables
-    for more information, see https://en.parceljs.org/env.html
-
-    These variables can be supplied to the Main.main function.
-    However, you will need to change the type to accept variables, by default it is an Effect.
-    You will probably want to make it a function from String -> Effect ()
-  */
-
-  Main.main();
+  document.addEventListener("DOMContentLoaded", function(){
+    // Clear the body dom before roloading app
+    window.document.getElementById('app').innerHtml = "";
+    Main.main();
+  });
 }
 
 // HMR setup. For more info see: https://parceljs.org/hmr.html
